8ea2786d7a5ff09b4583f758f0ceab6f8f6f372d
gabriel <gabriel.maurici@hotmail.com>
2024-11-11 11:57:57 -0300

    add timer expiração dos ingressos

--- front-end/src/pages/BuyTickets/BuyTickets.tsx
+++ front-end/src/pages/BuyTickets/BuyTickets.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import eventimSimulationApi from '../../services/eventim-simulaiton-api';
 import TicketsModal from '../../components/TicketsModal/TicketsModal';
@@ -19,10 +19,24 @@ interface Ticket {
 }
 
 const BuyTickets: React.FC = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { userToken } = useParams<{ userToken: string }>();
   const { eventId } = useParams<{ eventId: string }>();
   const [tickets, setTickets] = useState<Ticket[] | null>(null);
+  const [timeLeft, setTimeLeft] = useState(60);
+
+  useEffect(() => {
+    if (timeLeft <= 0) {
+      alert("O tempo para finalizar a compra expirou.");
+      navigate('/');
+    }
+
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => prevTime - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [timeLeft, navigate]);
 
   const handleConfirmPurchase = async () => {
     try {
@@ -49,6 +63,7 @@ const BuyTickets: React.FC = () => {
   return (
     <div className="buy-tickets-page">
       <div className="buy-tickets-container">
+        <div className="timer-box">Seus ingressos expiram em: {timeLeft} s</div>
         <h1>Finalizar Compra</h1>
         <p>Token do Usuário: {userToken}</p>
         <p>Revise suas informações e confirme a compra.</p>
